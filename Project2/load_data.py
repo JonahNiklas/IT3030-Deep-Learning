12665a8b72c9191a6dca42c7b53dddabe4c12296
jnwiecek <jnwiecek@stud.ntnu.no>
2024-03-17 22:41:51 +0100

    feat: add pred to fnn

--- Project2/load_data.py
+++ Project2/load_data.py
@@ -16,14 +16,13 @@ df['hour'] = pd.to_datetime(df['timestamp']).dt.hour
 
 
 df["consumption_t-1"] = df["NO1_consumption"].shift(1)
-
 # Reorder columns
 new_df = df[['year', 'month', 'day', 'hour', 'NO1_temperature',
              'consumption_t-1', 'NO1_consumption']]
 
 # remove the first row
 new_df = new_df.iloc[1:]
-new_df
+print(new_df.head())
 
 
 # Convert DataFrame to NumPy array
@@ -36,9 +35,9 @@ y = data[:, -1]
 
 # Perform train-validation-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42)
+    X, y, test_size=0.2, random_state=42, shuffle=False)
 X_train, X_val, y_train, y_val = train_test_split(
-    X_train, y_train, test_size=0.25, random_state=42)
+    X_train, y_train, test_size=0.25, random_state=42, shuffle=False)
 
 # Standardize the features using z-scores
 scaler = StandardScaler()
@@ -55,14 +54,13 @@ print("Train set shape:", X_train.shape, y_train.shape)
 print("Validation set shape:", X_val.shape, y_val.shape)
 print("Test set shape:", X_test.shape, y_test.shape)
 
-
 class TimeSeriesDataset(Dataset):
     def __init__(self, X, y, sequence_length):
         self.X = X
         self.X = [
             self.X[i:i + sequence_length] for i in range(len(self.X) - sequence_length+1)
         ]
-        self.y = y
+        self.y = y[sequence_length-1:]
 
     def __len__(self):
         return len(self.X)
@@ -70,25 +68,9 @@ class TimeSeriesDataset(Dataset):
     def __getitem__(self, idx):
         return tensor(self.X[idx]).float(), tensor(self.y[idx]).float()
 
-
-class DataFrameDataset(Dataset):
-    def __init__(self, dataframe, feature_columns, label_column, sequence_length):
-        self.data = dataframe[feature_columns]
-        self.num_features = len(feature_columns)
-        # convert data to sequences
-        self.data = [
-            self.data[i: i + sequence_length] for i in range(len(self.data) - sequence_length)
-        ]
-        self.labels = dataframe[label_column].view(-1, 1)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return tensor(self.data[idx]).float(), tensor(self.labels[idx]).float()
-
-
 def getTrainingSet(reshape=False, sequence_length=24):
+    if reshape:
+        return TimeSeriesDataset(X_train, y_train.reshape(-1,1), sequence_length)
     return TimeSeriesDataset(X_train, y_train, sequence_length)
 
 
@@ -101,3 +83,21 @@ def getTestSet(reshape=False, sequence_length=24):
 
 def getTestData():
     return X_test, y_test
+
+def createTestDataFromHoldOut(filepath):
+    df = pd.read_csv(filepath)
+    df = df.iloc[:, :3]
+    df['year'] = pd.to_datetime(df['timestamp']).dt.year
+    df['month'] = pd.to_datetime(df['timestamp']).dt.month
+    df['day'] = pd.to_datetime(df['timestamp']).dt.day
+    df['hour'] = pd.to_datetime(df['timestamp']).dt.hour
+    df["consumption_t-1"] = df["NO1_consumption"].shift(1)
+    new_df = df[['year', 'month', 'day', 'hour', 'NO1_temperature',
+                'consumption_t-1', 'NO1_consumption']]
+    new_df = new_df.iloc[1:]
+    data = new_df.to_numpy()
+    X = data[:, :-1]
+    y = data[:, -1]
+    X = scaler.transform(X)
+    y = y_scaler.transform(y.reshape(-1, 1)).flatten()
+    return X, y
